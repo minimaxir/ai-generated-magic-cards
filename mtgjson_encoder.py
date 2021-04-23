04d1b81fdc146dd88bfc90f8ebbeb9b0f6b29d62
Max Woolf <max@minimaxir.com>
2021-04-23 15:34:48 -0700

    Shuffle encoded texts before writing to csv

--- mtgjson_encoder.py
+++ mtgjson_encoder.py
@@ -33,14 +33,20 @@ with open(INPUT_PATH, "r", encoding="utf-8") as f:
         if card["name"] not in card_dict and not_invalid(card):
             card_dict[card["name"]] = {k: v for k, v in card.items() if k in FIELDS}
 
+enc_card_list = []
+
+for card in card_dict.values():
+    card["text"] = card["text"].replace(card["name"], "~")
+    for _ in range(REPETITIONS):
+        shuffle(FIELDS)
+        card_enc = "".join([f"<|{k}|>{card[k]}" for k in FIELDS])
+        # card_enc = re.sub(r"\d+", integer_to_carets, card_enc)
+        enc_card_list.append(card_enc.replace("[", "").replace("]", ""))
+
+shuffle(enc_card_list)
 
 with open(OUTPUT_PATH, "w", encoding="utf-8") as f:
     w = csv.writer(f)
     w.writerow(["text"])
-    for card in card_dict.values():
-        card["text"] = card["text"].replace(card["name"], "~")
-        for _ in range(REPETITIONS):
-            shuffle(FIELDS)
-            card_enc = "".join([f"<|{k}|>{card[k]}" for k in FIELDS])
-            # card_enc = re.sub(r"\d+", integer_to_carets, card_enc)
-            w.writerow([card_enc.replace("[", "").replace("]", "")])
+    for card in enc_card_list:
+        w.writerow([card])
